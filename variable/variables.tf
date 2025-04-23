8e375c6335a6700d2ded3b1f52f7a0832b495b7a
yohanse <mehabawyohanse793@gmail.com>
2025-04-24 00:55:04 +0300

    create variables

---
+++ variable/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_ip_address" {
+    default = "10.8.8.6/32"
+}
+
+variable "app_port" {
+    default = 8080
+}
+
+variable "ssh_port" {
+    default = 22
+}
+
+variable "ftp_port" {
+    default = 21
+}
