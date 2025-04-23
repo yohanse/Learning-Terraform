e12332951b0e7d6034868fe4e0c38d0ec62b6211
yohanse <mehabawyohanse793@gmail.com>
2025-04-24 00:14:34 +0300

    create ingress rule

--- cross-reference-attributes/main.tf
+++ cross-reference-attributes/main.tf
@@ -19,3 +19,12 @@ resource "aws_security_group" "terraform-SG" {
     name = "terraform-SG"
     description = "Adding security for learning purpose"
 }
+
+
+resource "aws_vpc_security_group_ingress_rule" "inbound_rule_one" {
+    security_group_id = aws_security_group.terraform-SG.id
+    from_port = 443
+    to_port = 443
+    ip_protocol = "tcp"
+    cidr_ipv4 = "${aws_eip.elastic_ip.public_ip}/32"
+}
