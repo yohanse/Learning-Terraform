d6dd53988195c25b65f5065d2f047eeb3ffb941c
yohanse <mehabawyohanse793@gmail.com>
2025-04-23 23:44:50 +0300

    creat instance

--- elastic-ip/main.tf
+++ elastic-ip/main.tf
@@ -12,3 +12,11 @@ resource "aws_eip" "elastic_ip" {
         Name = "Experiment elastic IP"
     }
 }
+
+resource "aws_instance" "eip_instance" {
+    ami = "ami-0e449927258d45bc4"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "Experment elastic IP"
+    }
+}
